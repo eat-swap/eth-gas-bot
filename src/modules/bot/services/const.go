afe8f62e9f688a99055261532a52bb18996e8621
Eat-Swap <root@eatswap.org>
2022-07-21 14:34:53 +0800

    add: basic incoming message handling

---
+++ src/modules/bot/services/const.go
@@ -0,0 +1,11 @@
+package services
+
+const (
+	DefaultMessage = "*Hi, %s\\!*\n" +
+		"I'm a bot that can help you find the cheapest gas price for your Ethereum transaction\\.\n\n" +
+		"To get started, here are some instructions:\n" +
+		"/help \\- Show this message\n" +
+		"/gas \\- Get the current gas price\n" +
+		"/eth \\- Get the current Ethereum price"
+	ErrorMessage = "Sorry, I am not able to understand this at the moment\\."
+)
--- src/modules/bot/services/handle.go
+++ src/modules/bot/services/handle.go
@@ -1,10 +1,57 @@
 package services
 
 import (
+	"eth-gas-bot/config"
 	"eth-gas-bot/modules/telegram/models/entities"
+	"eth-gas-bot/modules/telegram/models/params"
+	"eth-gas-bot/modules/telegram/services"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func HandleIncomingMessage(message *entities.Message, ctx *gin.Context) {
-	// Do nothing for now
+	var token = config.TelegramBotToken
+
+	switch message.Text {
+	default:
+		r, err := services.SendMessage(token, &params.SendMessageParams{
+			ChatId:                   message.Chat.Id,
+			Text:                     ErrorMessage,
+			ParseMode:                params.MessageParseModeMarkdown,
+			ReplyToMessageId:         message.MessageId,
+			AllowSendingWithoutReply: true,
+		})
+		if err != nil || r == nil {
+			log.Printf("Error sending message: %s", err)
+		}
+		fallthrough
+	case "/help":
+		r, err := services.SendMessage(token, &params.SendMessageParams{
+			ChatId:    message.Chat.Id,
+			Text:      fmt.Sprintf(DefaultMessage, message.Chat.FirstName),
+			ParseMode: params.MessageParseModeMarkdown,
+		})
+		if err != nil || r == nil {
+			log.Printf("Error sending message: %s", err)
+		}
+	case "/gas":
+		r, err := services.SendMessage(token, &params.SendMessageParams{
+			ChatId:    message.Chat.Id,
+			Text:      fmt.Sprintf("The current gas price is %s", "*Not implemented yet*"),
+			ParseMode: params.MessageParseModeMarkdown,
+		})
+		if err != nil || r == nil {
+			log.Printf("Error sending message: %s", err)
+		}
+	case "/eth":
+		r, err := services.SendMessage(token, &params.SendMessageParams{
+			ChatId:    message.Chat.Id,
+			Text:      fmt.Sprintf("The current Ethereum price is %s", "*Not implemented yet*"),
+			ParseMode: params.MessageParseModeMarkdown,
+		})
+		if err != nil || r == nil {
+			log.Printf("Error sending message: %s", err)
+		}
+	}
 }
